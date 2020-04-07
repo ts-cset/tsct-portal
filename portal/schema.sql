e99df4b72c47fd4c5dbb000bbbfda0476aa44c61
Cody Martin <codemart10@gmail.com>
2020-04-07 13:27:55 -0400

    added courses table added tests for courses table

--- portal/schema.sql
+++ portal/schema.sql
@@ -5,7 +5,9 @@
 -- command in your terminal.
 
 -- Drop existing tables
+DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
+
 
 -- Users
 CREATE TABLE users (
@@ -15,3 +17,13 @@ CREATE TABLE users (
     role varchar(7) NOT NULL CHECK (role IN ('teacher', 'student'))
 );
 
+-- Courses
+CREATE TABLE courses (
+  id bigserial PRIMARY KEY,
+  major text NOT NULL,
+  name text UNIQUE NOT NULL,
+  num integer UNIQUE NOT NULL,
+  description text,
+  credits integer NOT NULL,
+  teacher_id integer REFERENCES users (id)
+)
--- tests/data.sql
+++ tests/data.sql
@@ -4,3 +4,7 @@ INSERT INTO users (email, password, role)
 VALUES ('teacher@stevenscollege.edu', 'qwerty', 'teacher'),
        ('student@stevenscollege.edu', 'asdfgh', 'student');
 
+-- courses
+INSERT INTO courses (major, name, num, description, credits)
+VALUES ('CSET', 'Software Project II', 180, 'blaaah', 3),
+       ('CSET', 'Web Development', 160, 'web dev lol', 3);
