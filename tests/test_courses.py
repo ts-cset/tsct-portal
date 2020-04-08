caf2be1222ab07b74171e1dd6969d1a02870492e
Eric Moyer <ejmoyer3@comcast.net>
2020-04-08 13:37:11 -0400

    Finished the edit course test.

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,4 @@
 import pytest
-import psycopg2
 
 from portal.db import get_db
 
@@ -12,5 +11,17 @@ def test_create_course(app, client):
         response = client.post('/createcourse', data={'coursenumber': 100, 'major': 'CSET', 'coursename': 'test'})
         # check the db and see if that course exists now
         check = cur.execute("SELECT * FROM courses WHERE name = 'test';").fetchone()
+
+        assert check
+
+def test_edit_course(app, client):
+    with app.app_context():
+        db = get_db()
+
+        cur = db.cursor()
+        # post it again
+        response = client.post('/editcourse', data={'credits': 14})
+        # check if it's been updated
+        check = cur.execute("SELECT * FROM courses WHERE credits = 14")
 
         assert check
