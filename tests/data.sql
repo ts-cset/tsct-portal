4132061af6932d44cb664a74b8bf5938db7805c3
Cody Martin <codemart10@gmail.com>
2020-04-08 15:49:11 -0400

    added function in db.py function takes csv file and imports it added tests for the click command

--- tests/data.sql
+++ tests/data.sql
@@ -3,9 +3,8 @@
 INSERT INTO users (email, password, role)
 VALUES ('teacher@stevenscollege.edu', 'pbkdf2:sha256:150000$71PbcIFv$ffc17b1d44fa5ac0b5a9a1707b25e1c69b40a697676a6a30c374a57594b4df99', 'teacher'),
        ('student@stevenscollege.edu', 'pbkdf2:sha256:150000$bhcUxUAk$b08d717a84c93c4d09afe712f0d781b216e5b77a27b9becbf4d535fde22d0e97', 'student');
-       
+
 -- courses
 INSERT INTO courses (major, name, num, description, credits)
 VALUES ('CSET', 'Software Project II', 180, 'blaaah', 3),
        ('CSET', 'Web Development', 160, 'web dev lol', 3);
-
