5c5a33d34558fa76617cfc5774beebce471f3796
Cody Martin <codemart10@gmail.com>
2020-04-09 15:10:23 -0400

    added sessions and student_sessions table
    inserted mock data for both new tables

--- portal/schema.sql
+++ portal/schema.sql
@@ -5,8 +5,12 @@
 -- command in your terminal.
 
 -- Drop existing tables
+DROP TABLE IF EXISTS student_sessions;
+DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
+
+
 
 
 -- Users
@@ -14,20 +18,35 @@ CREATE TABLE users (
     id bigserial PRIMARY KEY,
     email text UNIQUE NOT NULL,
     password text NOT NULL,
-    name text,
+    name varchar(100),
     role varchar(7) NOT NULL CHECK (role IN ('teacher', 'student')),
-    major text
+    major varchar(4)
 );
-
-
 
 -- Courses
 CREATE TABLE courses (
   id bigserial PRIMARY KEY,
-  major text NOT NULL,
-  name text UNIQUE NOT NULL,
+  major varchar(4) NOT NULL,
+  name varchar(100) UNIQUE NOT NULL,
   num integer UNIQUE NOT NULL,
-  description text,
+  description varchar(1000),
   credits integer NOT NULL,
   teacher_id bigint REFERENCES users (id)
 );
+
+-- Session
+CREATE TABLE sessions (
+  id bigserial PRIMARY KEY,
+  course_id bigint REFERENCES courses (id),
+  teacher_id bigint REFERENCES users (id),
+  section varchar(1) NOT NULL,
+  meeting_time timestamptz NOT NULL,
+  location varchar(200)
+);
+
+-- Students sessions
+CREATE TABLE student_sessions (
+  id bigserial PRIMARY KEY,
+  session_id bigint REFERENCES sessions (id),
+  student_id bigint REFERENCES users (id)
+)
--- tests/data.sql
+++ tests/data.sql
@@ -5,6 +5,18 @@ VALUES ('teacher@stevenscollege.edu', 'pbkdf2:sha256:150000$71PbcIFv$ffc17b1d44f
        ('student@stevenscollege.edu', 'pbkdf2:sha256:150000$bhcUxUAk$b08d717a84c93c4d09afe712f0d781b216e5b77a27b9becbf4d535fde22d0e97', 'student');
 
 -- courses
-INSERT INTO courses (major, name, num, description, credits)
-VALUES ('CSET', 'Software Project II', 180, 'blaaah', 3),
-       ('CSET', 'Web Development', 160, 'web dev lol', 3);
+INSERT INTO courses (major, name, num, description, credits, teacher_id)
+VALUES ('CSET', 'Software Project II', 180, 'blaaah', 3, 1),
+       ('CSET', 'Web Development', 160, 'web dev lol', 3, 1);
+
+
+INSERT INTO sessions (course_id, teacher_id, section, meeting_time, location)
+VALUES (1, 1, 'A', 'now', 'here'),
+       (1, 1, 'B', 'now', 'place st'),
+       (2, 1, 'A', 'now', 'road st ave');
+
+
+INSERT INTO student_sessions (session_id, student_id)
+VALUES (1, 2),
+       (2, 2),
+       (3, 2);
