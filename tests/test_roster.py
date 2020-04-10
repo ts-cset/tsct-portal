c51743b047abdc45acd5192026a1f22ccd402f8c
Brendan Horst <yoshistar2000@gmail.com>
2020-04-10 14:11:15 -0400

    Wrote basic tests for the roster page

---
+++ tests/test_roster.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_roster_page(client):
+
+    response = client.get('/roster')
+    assert response.status_code == 200
+    assert b'CSET 180 Roster' in response.data
+
+def test_edit_roster(client):
+
+    #Check that a student appears in the roster list after being added
+    response = client.post('/roster', data={'email': 'student@stevenscollege.edu'})
+    assert b'student@stevenscollege.edu' in response.data
