ba43e8219fda483db262859c652e09a8bb37bda4
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-04-11 16:00:36 -0400

    bugfix for the test data

--- tests/data.sql
+++ tests/data.sql
@@ -1,5 +1,9 @@
 -- Mock Data For Tests
 
+INSERT INTO users (email, password, role)
+VALUES ('teacher@stevenscollege.edu', 'pbkdf2:sha256:150000$YKTR53od$3956727084a9d3470d800ca00005a2258bde5affab208eb9ae211c46a28f575d', 'teacher'),
+       ('student@stevenscollege.edu', 'pbkdf2:sha256:150000$db49oioE$95b37dfa834211006b45fddfa03debdca5d2ccf49486bb7e35af144406d51c8a', 'student');
+
 INSERT INTO courses (name, major, description, teacherid)
 VALUES ('ENG 101', 10, 'English Composition: Basic Writing', 42267),
       ('METAL 155', 9, 'Basic metals kinda stuff', 53114),
