95510361241ce9df188f5551fd4f763bf0e32320
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-04-11 16:24:27 -0400

    CHAGE MOCK-DB DATA FOR TESTING
    I had to change the mock teacher ids in the courses table to reflect the mock teachers

--- tests/data.sql
+++ tests/data.sql
@@ -5,11 +5,11 @@ VALUES (0001, 'teacher@stevenscollege.edu', 'qwerty', 'test', 'teacher', 1),
        (0002, 'student@stevenscollege.edu', 'password', 'test', 'student', 10);
 
 INSERT INTO courses (name, major, description, teacherid)
-VALUES ('ENG 101', 10, 'English Composition: Basic Writing', 42267),
-      ('METAL 155', 9, 'Basic metals kinda stuff', 53114),
-      ('CSET 180', 4, 'This damn software project', 52502),
-      ('DRAW 101', 1, 'Drawing basic buildings', 42267),
-      ('ENG 201', 10, 'SECOND YEAR English Composition: Basic Writing', 42267),
-      ('METAL 255', 9, 'SECOND YEAR Basic metals kinda stuff', 53114),
-      ('CSET 280', 4, 'SECOND YEAR This damn software project', 52502),
-      ('DRAW 201', 1, 'SECOND YEAR Drawing basic buildings', 42267);
+VALUES ('ENG 101', 10, 'English Composition: Basic Writing', 0001),
+      ('METAL 155', 9, 'Basic metals kinda stuff', 0001),
+      ('CSET 180', 4, 'This damn software project', 0001),
+      ('DRAW 101', 1, 'Drawing basic buildings', 0001),
+      ('ENG 201', 10, 'SECOND YEAR English Composition: Basic Writing', 0002),
+      ('METAL 255', 9, 'SECOND YEAR Basic metals kinda stuff', 0002),
+      ('CSET 280', 4, 'SECOND YEAR This damn software project', 0002),
+      ('DRAW 201', 1, 'SECOND YEAR Drawing basic buildings', 0002);
