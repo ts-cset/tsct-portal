9a7e08aaf761968388791816719722490af7b145
Charles Max Crandall <robloxatme.com@gmail.com>
2020-04-14 15:41:06 -0400

    You can no longer create duplicate courses You can no longer update a course to be a duplicate of another course Updated the html of the course pages Updated tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,24 +9,22 @@ def test_login(client):
         '/auth/login', data={'email': 'teacher@stevenscollege.edu', 'password':'qwerty'}
     )
     response = client.get('/portal/userpage')
-    assert b'<p>You are logged in</p>' in response.data
     assert b'teacher@stevenscollege.edu' in response.data
     assert b'Log In' not in response.data
     assert b'Log Out' in response.data
     assert b'User Page' in response.data
-    assert b'You are a teacher' in response.data
+    assert b'Courses' in response.data
     response = client.get('/auth/logout')
     response = client.get('/auth/login')
     response = client.post(
         '/auth/login', data={'email': 'student@stevenscollege.edu', 'password':'asdfgh'}
     )
     response = client.get('/portal/userpage')
-    assert b'<p>You are logged in</p>' in response.data
     assert b'student@stevenscollege.edu' in response.data
     assert b'Log In' not in response.data
     assert b'Log Out' in response.data
     assert b'User Page' in response.data
-    assert b'You are a teacher' not in response.data
+    assert b'Courses' in response.data
 
 def test_login_fail(client):
     response = client.get('/')
@@ -58,4 +56,3 @@ def test_logout(client):
     assert b'Log In' in response.data
     assert b'Log Out' not in response.data
     assert b'User Page' not in response.data
-    
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,7 +6,7 @@ def test_view_course(client):
         '/auth/login', data={'email': 'teacher@stevenscollege.edu', 'password':'qwerty'}
     )
     response = client.get('/portal/userpage')
-    response = client.get('/portal/courses/courses')
+    response = client.get('/portal/courses/')
     assert b'101' in response.data
     assert b'Web Design' in response.data
     assert b'CSET' in response.data
@@ -24,7 +24,7 @@ def test_create_course(client):
     response = client.post(
         '/portal/courses/create-course', data={'course_number': '102', 'name':'CSET1', 'description':'class', 'credits':'9'}
     )
-    response = client.get('/portal/courses/courses')
+    response = client.get('/portal/courses/')
     assert b'101' in response.data
     assert b'CSET1' in response.data
     assert b'CSET' in response.data
@@ -40,14 +40,14 @@ def test_update_course(client):
     response = client.post(
         '/portal/courses/create-course', data={'course_number': '102', 'name':'CSET2', 'description':'class', 'credits':'10'}
     )
-    response = client.get('/portal/courses/courses')
+    response = client.get('/portal/courses/')
     assert b'102' in response.data
     assert b'CSET2' in response.data
     assert b'CSET' in response.data
     response = client.post(
         '/portal/courses/update-course/3', data={'course_number': '103', 'name':'CSET3', 'description':'class1', 'credits':'11'}
     )
-    response = client.get('/portal/courses/courses')
+    response = client.get('/portal/courses/')
     assert b'102' not in response.data
     assert b'CSET2' not in response.data
     assert b'103' in response.data
