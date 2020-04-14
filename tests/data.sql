98cbedb21e417f29a48cf754fdbc447b948e58e8
TheIrishBub <theirishbub@hotmail.com>
2020-04-14 13:38:22 -0400

    Fixed IDs for mock users

--- tests/data.sql
+++ tests/data.sql
@@ -5,5 +5,5 @@ VALUES ('CSET'),
         ('ARCH');
 
 INSERT INTO users (id, email, password, name, role, major)
-VALUES (12345, 'teacher@stevenscollege.edu', 'qwerty', 'Teacher', 'teacher', 1),
-       (54321, 'student@stevenscollege.edu', 'asdfgh', 'Student', 'student', 1);
+VALUES (1, 'teacher@stevenscollege.edu', 'qwerty', 'Teacher', 'teacher', 1),
+       (2, 'student@stevenscollege.edu', 'asdfgh', 'Student', 'student', 1);
