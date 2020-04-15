69e84eeb34d93ea0769991b29a401d9f1482dcae
Brendan Horst <yoshistar2000@gmail.com>
2020-04-15 13:50:01 -0400

    Started roster input validation and corresponding tests

--- portal/roster.py
+++ portal/roster.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, session, g, flash
+from flask import render_template, Blueprint, session, g, flash, request
 
 from . import db
 
@@ -9,8 +9,8 @@ bp = Blueprint("roster", __name__)
 def display_roster():
 
     #Test variables
-    course = { 'name': 'CSET 180'}
-    session = { 'name': 'A'}
+    course = { 'name': 'CSET 180' }
+    session = { 'name': 'A' }
     students = [
         {
             'name': 'John Smith',
@@ -18,9 +18,50 @@ def display_roster():
         },
         {
             'name': 'Example McTest',
-            'email': 'student@stevenscollege.edu'
+            'email': 'example@stevenscollege.edu'
         }
     ]
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        already_enrolled = False
+        error = None
+
+        with db.get_db() as con:
+            with con.cursor() as cur:
+
+                # Find the student in the users table
+                cur.execute("""SELECT name, email, role FROM users
+                    WHERE email = %s""", (email,))
+
+                user = cur.fetchone()
+
+                # Check if the student is already in the current session
+
+        # If there is no student with the entered email create an error message
+        if user == None:
+
+            error = 'No student found'
+
+        # If the selected user is not a student, create an error message
+        if user['role'] != 'student':
+
+            error = f'{user['name']} is not a student'
+
+        # If the student is already in the session create an error message
+        if already_enrolled:
+
+            error = f'{user['name']} is already enrolled in this session.'
+
+        if error == None:
+
+            #Insert user into the roster
+            pass
+
+        else:
+
+            flash(error)
 
 
     return render_template('roster.html', course=course, session=session, students=students)
--- tests/test_roster.py
+++ tests/test_roster.py
@@ -11,3 +11,14 @@ def test_edit_roster(client):
     #Check that a student appears in the roster list after being added
     response = client.post('/roster', data={'email': 'student@stevenscollege.edu'})
     assert b'student@stevenscollege.edu' in response.data
+
+@pytest.mark.parametrize(('email', 'message'), (
+    ('incorrect@email.com', b'No student found'),
+    ('teacher@stevenscollege.edu', b'Teacher is not a student'),
+    ('student2@stevenscollege.edu', b'Student2 is already registered')
+))
+def test_roster_validation(client, email, message):
+
+    response = client.post('/roster', data={'email': email })
+
+    assert message in resposne.data
