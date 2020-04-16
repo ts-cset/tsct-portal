0d5e9f4b347113cca9e920f0aaa44966550e605b
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-04-16 12:25:39 -0400

    SESSIONS - CREATE/EDIT/DELETE
    added functions and routes to see, edit, and create sessions
    
    added html in order to view sessions

--- portal/portal.py
+++ portal/portal.py
@@ -140,13 +140,63 @@ def create():
 @bp.route("/<int:id>/sessions", methods=['GET', 'POST'])
 @login_required
 def view_sessions(id):
-    """Single page view of course"""
+    """Single page view of session"""
     cur = db.get_db().cursor()
-    cur.execute("""SELECT * FROM sessions where course = %s""",
+    cur.execute("""SELECT sessions.id, sessions.course, sessions.days, sessions.class_time, courses.teacherid AS teacher_id FROM sessions FULL JOIN courses ON courses.course_id = sessions.course WHERE sessions.course = %s""",
                 (id,))
-    # cur.execute("""SELECT sessions.course, sessions.days, sessions.class_time, courses.teacherid, users.name AS teacher_name FROM sessions INNER JOIN users ON courses.teacherid = users.id WHERE courses.course_id = %s""",
-    #             (id,))
     sessions = cur.fetchall()
     cur.close()
 
     return render_template("layouts/sessions/view_sessions.html", sessions=sessions)
+
+
+@bp.route("/sessions/create", methods=['GET', 'POST'])
+@login_role
+@login_required
+def create(id):
+
+    if request.method == 'POST':
+
+        teacherid = session['user_id']
+        session_days = request.form['session_days']
+        session_time = request.form['session_time']
+        cur = db.get_db().cursor()
+        cur.execute("""
+        INSERT INTO sessions (course, teacher, days, class_time)
+        VALUES (%s, %s, %s, %s)""",
+                    (id, teacherid, session_days, session_time,))
+
+        g.db.commit()
+        return redirect(url_for('portal.view', id=id))
+
+    return render_template("layouts/sessions/create_sessions.html")
+
+
+@bp.route("/<int:id>/sessions/edit", methods=['GET', 'POST'])
+@login_required
+@login_role
+def edit_session(id):
+    """Edit a session"""
+    cur = db.get_db().cursor()
+    cur.execute("""SELECT * FROM sessions WHERE id = %s""",
+                (id,))
+    session = cur.fetchone()
+    cur.close()
+
+    if request.method == 'POST':
+
+        session_days = request.form['session_days']
+        session_time = request.form['session_time']
+
+        cur = db.get_db().cursor()
+        cur.execute(
+            'UPDATE sessions SET days = %s, class_time = %s'
+            ' WHERE id = %s ',
+            (session_days, session_time, id)
+        )
+        g.db.commit()
+        cur.close()
+
+        return redirect(url_for('portal.view_sessions', id=session['course']))
+
+    return render_template("layouts/sessions/edit_session.html", session=session)
