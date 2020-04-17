8de4aa7727fcccb5d36ce9274529a455f5d07c71
Melaku <alebachewtegegne2017@yahoo.com>
2020-04-17 13:56:09 -0400

    add a note in the test file

--- tests/test_course.py
+++ tests/test_course.py
@@ -32,11 +32,14 @@ def test_edit(client, auth):
     response = client.get('/home')
     assert b'CSET 180'in response.data
 def test_delete(client, auth):
+    # login to the page
     auth.login()
     response = client.post('/1/delete')
     assert response.headers['Location'] == 'http://localhost/home'
 
 def test_view(client, auth):
-    auth.login()
+    # login to the page
+    auth.login()]
+    #get the course by clicking the view button
     response = client.post('/2/view')
     assert b'Course Information' in response.data
