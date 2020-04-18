38bf7188ecd55d9fd8afaaff0f5ebc36067b0c11
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-04-18 14:22:38 -0400

    ADDED HASHED PASSWORD FOR STUDENT
    added hased password for student and created a second student in the mock data

--- tests/data.sql
+++ tests/data.sql
@@ -1,9 +1,10 @@
 -- Mock Data For Tests
 
 INSERT INTO users (id, email, password, name, role, major)
-VALUES (0001, 'teacher@stevenscollege.edu', '$2b$12$xVtl1ZGJBEW4gxQOm9UzW.P.UOq/cR7/TEv.G3l3x.TmV29yX8a5y', 'test teacher', 'teacher', 1),
-       (0002, 'student@stevenscollege.edu', 'password', 'test student', 'student', 10),
-       (0003, 'teacher2@stevenscollege.edu', '$2b$12$xVtl1ZGJBEW4gxQOm9UzW.P.UOq/cR7/TEv.G3l3x.TmV29yX8a5y', 'test teacher 2', 'teacher', 2);
+VALUES (0001, 'teacher@stevenscollege.edu', '$2b$12$xVtl1ZGJBEW4gxQOm9UzW.P.UOq/cR7/TEv.G3l3x.TmV29yX8a5y', 'Test Teacher', 'teacher', 1),
+       (0002, 'student@stevenscollege.edu', '$2b$12$PE/vM8jOb6Og84zdJ9GvKe8177OZ8EhMRzEwAONF1R8R6qeJ6PAsm', 'Test Student', 'student', 10),
+       (0003, 'teacher2@stevenscollege.edu', '$2b$12$xVtl1ZGJBEW4gxQOm9UzW.P.UOq/cR7/TEv.G3l3x.TmV29yX8a5y', 'Test Teacher 2', 'teacher', 2)
+       (0004, 'student2@stevenscollege.edu', '$2b$12$PE/vM8jOb6Og84zdJ9GvKe8177OZ8EhMRzEwAONF1R8R6qeJ6PAsm', 'Test Student 2', 'student', 3);
 
 INSERT INTO courses (name, major, description, teacherid)
 VALUES ('ENG 101', 'GEND', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper quam et quam fringilla feugiat. Donec at risus efficitur, vehicula risus et, tempor nibh. Vivamus vitae porttitor metus, ac venenatis quam. Pellentesque porttitor malesuada orci iaculis condimentum. Vestibulum sodales, purus sit amet ultricies luctus, leo arcu mattis dui, at ultrices tortor eros sit amet lorem. Duis quis metus fringilla neque ornare ornare. Sed commodo sit amet elit et dictum. Nulla eget mattis ligula. Nulla sodales enim nec leo eleifend, et feugiat felis fermentum. Cras aliquet a magna ac pellentesque. Nulla ultrices bibendum dui tristique facilisis. Nam pellentesque lobortis ultricies. In id pretium quam. Sed facilisis lacinia lectus at tristique.', 0003),
