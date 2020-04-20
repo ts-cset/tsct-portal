ea0f90aeaada7e79ceb2516138f63238937ca067
Steve Eckles <steve.d.eckles@gmail.com>
2020-04-20 15:42:17 -0400

    Move some temporary test data out of data.sql

--- tests/data.sql
+++ tests/data.sql
@@ -17,16 +17,3 @@ VALUES ('Big Software', 'You need to create some big software with lots of big s
 
 INSERT INTO session_assignments (session_id, assignment_id, due_date)
 VALUES (1, 1, '4/25/2020');
-
--- ID,EMAIL,PASSWORD,NAME,ROLE,MAJOR
--- 42267,duck@stevenscollege.edu,looney,Daffy Duck,teacher,CSET
--- 53114,--email doo@stevenscollege.edu --password snacks --first Scooby --last Doo --role teacher --major ARCH
--- 52502,--email mouse@stevenscollege.edu --password disney --first Minnie --last Mouse --role teacher --major CSET
--- 13041,--email bsimpson041@stevenscollege.edu --password iwillnot --first Bart --last Simpson --role student --major ARCH
--- 36706,--email lsimpson706@stevenscollege.edu --password saxophone --first Lisa --last Simpson --role student --majorCSET
--- 70128,cbrown128@stevenscollege.edu,goodgrief,Charlie Brown,student,CSET
--- 25208,apickles208@stevenscollege.edu,spoiled,Angelica Pickles,student,CSET
--- 48175,bbunny175@stevenscollege.edu,carrots,Bugs Bunny,student,ARCH
--- 50425,fflintstone425@stevenscollege.edu,bowling,Fred Flintstone,student,ARCH
-
--- heroku run -a desolate-cliffs-09215 flask register --email lsimpson706@stevenscollege.edu --password saxophone --first Lisa --last Simpson --role student --major CSET
