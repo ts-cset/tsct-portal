70e0ee6c0dd03a8027aa4d8f08f35ed4f941a885
Scott Johnson <scottyjohnson002@gmail.com>
2020-04-22 15:54:47 -0400

    sessions can be deleted by the teacher

--- portal/sessions.py
+++ portal/sessions.py
@@ -55,7 +55,6 @@ def sessions():
         return render_template('portal/sessions.html', sessions=classes, sections=sections, course_id=course_id)
 
 
-
 @bp.route('/createsession', methods=("GET", "POST"))
 def session_create():
     """View for creating a session"""
@@ -79,9 +78,23 @@ def session_create():
         students = request.form.getlist('students')
 
         cur = get_db().cursor()
+        cur.execute(
+            """SELECT * FROM sessions WHERE course_id = %s and section = %s;""", (course_id, section))
+        existingsection = cur.fetchall()
+        if existingsection != []:
+            error = 'Section Already Exists'
+            # TODO: don't remove entered info
+            session['section'] = section
+            session['meeting'] = meeting_time
+            session['location'] = location
+            flash(error)
+            return render_template('portal/createsession.html', all_students=all_students)
         cur.execute("""INSERT INTO sessions (course_id,section, meeting_time, location, teacher_id)
                         VALUES (%s, %s, %s, %s, %s);""", (course_id, section, meeting_time, location, teacher_id))
         get_db().commit()
+        session['section'] = ''
+        session['meeting'] = ''
+        session['location'] = ''
 
         for student in students:
             # find student in user db then grab it by name
@@ -100,40 +113,41 @@ def session_create():
     return render_template('portal/createsession.html', all_students=all_students)
 
 
-@bp.route('/<int:session_id>/editsession', methods=("GET", "POST"))
-def session_edit(session_id):
-    """Edits the course name/info"""
-    cur = get_db().cursor()
-    teacher = session['user'][0]
-    cur.execute("SELECT * FROM sessions WHERE teacher_id = %s AND id = %s;",
-                (teacher, session_id))
-    selected_session = cur.fetchone()
+# @bp.route('/<int:course_id>/<section>/editsession', methods=("GET", "POST"))
+# def session_edit(course_id, section):
+#     """Edits the session name/info"""
+#     cur = get_db().cursor()
+#     teacher = session['user'][0]
+#     cur.execute("SELECT * FROM sessions WHERE section= %s AND course_id = %s;",
+#                 (section, course_id))
+#     selected_session = cur.fetchone()
+#
+#     if session['user'][4] == 'teacher':
+#         return render_template('portal/home.html')
+#
+#     if request.method == "POST":
+#         section = request.form['section']
+#         meeting_time = request.form['meeting']
+#         location = request.form['location']
+#         teacher_id = session['user'][0]
+#         students = request.form.getlist('students')
+#
+#         cur.execute("DELETE FROM student_sessions WHERE section = %s AND course_id = %s;",
+#                     (section, course_id))
+#         # Remove all students from previous sessions
+#
+#         cur.execute(
+#             """UPDATE sessions SET (course_id,section, meeting_time, location, teacher_id) = (%s, %s, %s, %s, %s)
+#                 WHERE section = %s AND course_id = %s ;""", (course_id, section, meeting_time, location, teacher_id, section, course_id)
+#         )
+#
+#         get_db().commit()
+#         cur.close()
+#
+#         return redirect(url_for('courses.courses'))
+#
+#     return render_template("portal/editsession.html", course=course)
 
-    if session['user'][4] == 'teacher':
-
-        if request.method == "POST":
-            section = request.form['section']
-            meeting_time = request.form['meeting']
-            location = request.form['location']
-            teacher_id = session['user'][0]
-            students = request.form.getlist('students')
-
-            cur.execute("DELETE FROM student_sessions WHERE session_id = %s;",
-                        (session_id,))
-            # Update the course
-
-            cur.execute(
-                """UPDATE courses SET (major, name, num, credits, description) = (%s, %s, %s, %s, %s)
-                    WHERE id = %s AND teacher_id = %s ;""", (cour_maj, cour_name, cour_num, cour_cred, cour_desc, cour_id, teacher)
-            )
-            get_db().commit()
-            cur.close()
-
-            return redirect(url_for('courses.courses'))
-
-        return render_template("portal/editcourse.html", course=course)
-    else:  # if not a teacher, send them to home page
-        return render_template('portal/home.html')
 
 @bp.route('/viewsession', methods=('GET', 'POST'))
 def session_view():
@@ -143,8 +157,42 @@ def session_view():
     section = request.args.get("section")
     classname = request.args.get("class")
 
-    cur.execute("SELECT * FROM sessions WHERE course_id = %s AND section = %s;", (course_id, section))
+    cur.execute(
+        "SELECT * FROM sessions WHERE course_id = %s AND section = %s;", (course_id, section))
     session_info = cur.fetchone()
     print(session_info)
 
     return render_template('portal/viewsession.html', course_id=course_id, section=section,  classname=classname, session_info=session_info)
+
+
+@bp.route('/deletesession', methods=("POST",))
+def session_delete():
+    """View for deleting session"""
+    if session['user'][4] != 'teacher':
+        return render_template('portal/home.html')  # if the are a teacher
+        # TODO: check teacher id
+    session_info = request.form['session_to_delete']
+    course_id = request.form['course_id']
+    section = request.form['section']
+    teacher = session['user'][0]
+    cur = get_db().cursor()
+    cur.execute("SELECT * FROM sessions WHERE  course_id= %s and section = %s",
+                (course_id, section))
+    session_information = cur.fetchone()
+    if session_information['teacher_id'] != teacher:
+        return render_template('portal/home.html')
+
+    if request.method == 'POST':
+        cur.execute("DELETE FROM assignments WHERE  course_id= %s and section = %s;",
+                    (course_id, section))
+        get_db().commit()
+        cur.execute("DELETE FROM student_sessions WHERE  course_id= %s and section = %s;",
+                    (course_id, section))
+        get_db().commit()
+        cur.execute("DELETE FROM sessions WHERE course_id = %s and section = %s;",
+                    (course_id, section))
+        get_db().commit()
+
+        return redirect(url_for('courses.courses'))
+    else:  # if not a teacher, send to home
+        return render_template('portal/home.html')
