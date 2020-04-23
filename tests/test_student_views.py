79b21404d25cddf599731e3fc3f5432e74573a32
TheIrishBub <theirishbub@hotmail.com>
2020-04-23 15:06:15 -0400

    Added another test for sessions assignments view

--- tests/test_student_views.py
+++ tests/test_student_views.py
@@ -23,17 +23,32 @@ def test_schedule_view(client):
     rv = logout(client)
     assert b'TSCT Portal Login' in rv.data
 
-def test_assign_view(client):
-    assert client.get('/your_assignments').status_code == 302
+def test_session_assignments(client):
+    assert client.get('/your_assignments/session/1').status_code == 302
     # login to check database
     rv = login(client, 'student@stevenscollege.edu', 'asdfgh')
     assert b'Logged in' in rv.data
     # go to schedule view to test data on page
-    assert client.get('/your_assignments').status_code == 200
-    response = client.get('/your_assignments')
+    assert client.get('/your_assignments/session/1').status_code == 200
+    response = client.get('/your_assignments/session/1')
     # test to see if mock data is on page, and see if the location/url is correct
-    assert b'Assignment Name' in response.data
-    assert b'Exam 1' in response.data
+    assert b'Your Assignments' in response.data
+    assert b'test1' in response.data
+    # test logging out
+    rv = logout(client)
+    assert b'TSCT Portal Login' in rv.data
+
+def test_assign_view(client):
+    assert client.get('/session/1/assignment_details/2').status_code == 302
+    # login to check database
+    rv = login(client, 'student@stevenscollege.edu', 'asdfgh')
+    assert b'Logged in' in rv.data
+    # go to schedule view to test data on page
+    assert client.get('/session/1/assignment_details/2').status_code == 200
+    response = client.get('/session/1/assignment_details/2')
+    # test to see if mock data is on page, and see if the location/url is correct
+    assert b'Title' in response.data
+    assert b'test1' in response.data
     # test logging out
     rv = logout(client)
     assert b'TSCT Portal Login' in rv.data
