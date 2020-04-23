b99c284b170bd7e8472b9d7f3fc269deca2e269d
thienqui-communistspy <professionalbusiness505@gmail.com>
2020-04-23 17:00:00 -0400

    Added grading feature

--- portal/schema.sql
+++ portal/schema.sql
@@ -10,7 +10,8 @@ DROP TABLE IF EXISTS courses CASCADE;
 DROP TABLE IF EXISTS sessions CASCADE;
 DROP TABLE IF EXISTS roster;
 DROP TABLE IF EXISTS assignments CASCADE;
-DROP TABLE IF EXISTS session_assignments;
+DROP TABLE IF EXISTS session_assignments CASCADE;
+DROP TABLE IF EXISTS assignment_grades CASCADE;
 
 -- Users
 CREATE TABLE users (
@@ -65,9 +66,18 @@ CREATE TABLE assignments (
 -- Session Assignments
 -- Create a many-to-many relationship between course sessions and assignments for those sessions
 CREATE TABLE session_assignments (
+  work_id bigserial PRIMARY KEY,
   -- Create a one-to-many relationship between sessions and assignments
   session_id bigint REFERENCES sessions(id) NOT NULL,
   -- Create a one-to-many relationship between assignments and sessions
   assignment_id bigint REFERENCES assignments(id) NOT NULL,
+  --Create a one to many relationsh
   due_date date
+  --Create a one to many relationship between the assignment and the student
+);
+
+CREATE TABLE assignment_grades(
+owner_id bigint REFERENCES users(id) NOT NULL,
+assigned_id bigint REFERENCES session_assignments(work_id) NOT NULL,
+grades varchar(100)
 );
--- tests/data.sql
+++ tests/data.sql
@@ -5,18 +5,24 @@ VALUES ('teacher@stevenscollege.edu', 'pbkdf2:sha256:150000$YKTR53od$3956727084a
        ('student@stevenscollege.edu', 'pbkdf2:sha256:150000$db49oioE$95b37dfa834211006b45fddfa03debdca5d2ccf49486bb7e35af144406d51c8a', 'student', 'Lueklee', 'Kevstice', 'CSET');
 
 INSERT INTO courses (course_code, course_name, major, description, teacher_id)
-VALUES ('180', 'Big Software Energy', 'CSET', 'A class that really makes you FEEL like a developer', 1);
-
+VALUES ('180', 'Big Software Energy', 'CSET', 'A class that really makes you FEEL like a developer', 1),
+       ('500', 'Brick smashing', 'MASN', 'Smashing bricks and smashing brick sciences', 1);
 INSERT INTO sessions (session_name, meeting_days, course_id)
-VALUES ('A', 'MTWThF', 1);
+VALUES ('A', 'MTWThF', 1),
+       ('B', 'MT', 2);
 
 INSERT INTO assignments (name, description, points, course_id)
 VALUES ('Big Software', 'You need to create some big software with lots of big software energy', 200, 1),
        ('Bigger Software', 'You need to create some big software with lots of big software energy', 300, 1),
        ('Biggest Software', 'You need to create some big software with lots of big software energy', 400, 1),
        ('Mondo Software', 'Very mondo my dude', 25, 1);
+       ('Breaking bricks', 'Break a brick on your head', 600,2);
 
 INSERT INTO session_assignments (session_id, assignment_id, due_date)
 VALUES (1, 1, '4/25/2020'),
        (1, 2, '4/30/2020'),
-       (1, 3, '5/02/2020');
+       (1, 3, '5/02/2020'),
+       (2, 4, '08/01/2020');
+INSERT INTO roster(student_id, session_id)
+VALUES(2, 1),
+      (2, 2)
