b6a326b4d03514365515b53f56dde7cc314e3dde
Eric Moyer <ejmoyer3@comcast.net>
2020-04-23 14:54:08 -0400

    Started to replace session calls in code with much safer g object calls. Changed htmls to use g instead of sessions. Added a teacher_login and login_required decorator. Added load logged in user to store the user in a safer way.

--- portal/auth.py
+++ portal/auth.py
@@ -1,5 +1,6 @@
 import os
 from . import db
+import functools
 
 from flask import (
     Flask, Blueprint, flash, g, redirect, render_template, request, url_for, session
@@ -7,6 +8,38 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('auth', __name__)
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    cur = db.get_db().cursor()
+    if user_id is None:
+        g.user = None
+    else:
+        cur.execute('SELECT * FROM users WHERE id = %s;', (user_id,))
+        g.user = cur.fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.index'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+def teacher_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.index'))
+        if g.user['role'] != 'teacher':
+            return redirect(url_for('auth.home'))
+
+        return view(**kwargs)
+
+    return wrapped_view
 
 #------- Index -----------------------------------------------------------------
 @bp.route('/', methods=('GET', 'POST'))
@@ -17,14 +50,14 @@ def index():
         cur = db.get_db().cursor()
         error = None
         cur.execute(
-            'SELECT * FROM users WHERE email = %s', (email,)
+            'SELECT * FROM users WHERE email = %s;', (email,)
         )
         user = cur.fetchone()
         if user is None or not check_password_hash(user['password'], password):
             error = 'Incorrect email or password.'
         if error is None:
             session.clear()
-            session['user'] = user
+            session['user_id'] = user['id']
             return redirect(url_for('auth.home'))
 
         flash(error)
