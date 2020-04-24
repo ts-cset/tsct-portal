2c20c2746b8f422de8623a294540883f4e8cef35
Melaku <alebachewtegegne2017@yahoo.com>
2020-04-24 14:08:02 -0400

    test the student-grade view file

--- tests/test_student.py
+++ tests/test_student.py
@@ -3,4 +3,19 @@ import pytest
 def test_student_classes(client , auth):
     auth.student_login()
     response = client.get('/student')
-    assert b'Student Homepage' and b'METAL 255' in response.data
+    assert b'Student Homepage' in response.data
+    assert b'METAL 255' in response.data
+def test_student_grade(client , auth):
+    auth.student_login()
+    response = client.get('/student/course/3/session/1/assignment/1/grade')
+    assert b'Assignment Grades' in response.data
+def test_view_student_gradebook(client , auth):
+    auth.student_login()
+    response = client.get('/student/gradebook')
+    assert b'Student Gradebook' in response.data
+    assert b'All Student Courses' in response.data
+def test_view_grades_by_course(client , auth):
+    auth.student_login()
+    response = client.get('/student/gradebook/course/3')
+    assert b'Course Grades' in response.data
+    assert b'CSET 180' in response.data
