3b3c6e57ed59169b79775eb7758ed38bfe0af5d5
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-04-28 14:31:51 -0400

    fixed assignment test

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -1,32 +1,36 @@
 import pytest
 
+
 def test_create_assignment(client, auth):
     auth.login()
-    #check the main assignment page works
+    # check the main assignment page works
     response = client.get('/course/2/session/2/assignments')
     assert b'Assignments' in response.data
-    #check the create page works
+    # check the create page works
     response = client.get('/course/2/session/2/create_assignment')
     assert b'Create Assignment' in response.data
     response = client.post('/course/2/session/2/create_assignment', data={
-    'name': 'Magic',
-    'description': 'It is leviOsa not LevioSA',
-    'date': '2020-05-25'})
+        'name': 'Magic',
+        'description': 'It is leviOsa not LevioSA',
+        'date': '2020-05-25',
+        'total_points': '30'})
     assert '/course/2/session/2/assignments' in response.headers['location']
+
 
 def test_edit_assignment(client, auth):
     auth.login()
-    #Make sure the link to the edit works
+    # Make sure the link to the edit works
     response = client.get('/course/2/session/2/assignment/3/edit-assignment')
     assert b'Edit Assignment: 3' in response.data
-    #Edit some data with other data
+    # Edit some data with other data
     response = client.post('/course/2/session/2/assignment/3/edit-assignment', data={
-    'name': 'Magic2',
-    'description': 'Who is this?',
-    'date': '2020-05-25'})
+        'name': 'Magic2',
+        'description': 'Who is this?',
+        'date': '2020-05-25'})
     assert '/course/2/session/2/assignments' in response.headers['Location']
     response = client.get('/course/2/session/2/assignments')
     assert b'Magic2' in response.data
+
 
 def test_delete_assignment(client, auth):
     auth.login()
