4399de26907d93e9f850cc60297a2e6ecf4da1a2
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-05-01 08:24:10 -0400

    COURSE GRADEBOOK
    changed the course gradebook page to return as a table for a more consistent look to the rest of the site
    modified test data so all assignment total points were the same for each assignment for consistency
    added round to the course grade function so the grade percentage would come out to 2 decimal places
    got the links to individual assignments working again from the gradebook

--- portal/student.py
+++ portal/student.py
@@ -11,6 +11,7 @@ bp = Blueprint("student", __name__)
 @login_required
 @student_required
 def student_view():
+    """shows student a list of all their current courses and major on the homepage"""
     sessions = []
     user_id = session.get('user_id')
     con = db.get_db()
@@ -42,15 +43,9 @@ def assignment_grade(id, session_id, course_id):
 
     user_id = session.get('user_id')
 
-
-<< << << < HEAD
     con = db.get_db()
     cur = con.cursor()
-    cur.execute("""SELECT (ROUND(grades.points_received/grades.total_points, 2 )*100) as assignment_grade,
-=======
-    cur = db.get_db().cursor()
     cur.execute("""SELECT DISTINCT(ROUND(grades.points_received / grades.total_points, 2) * 100) as assignment_grade,
->>>>>> > 38fcd03cf03fb074bfe4016a0695b48dc5075de0
                 grades.total_points as total, grades.points_received as earned,
                 grades.submission as submission, grades.feedback as feedback,
                grades.student_id, grades.assignment_id as assign_id, assignments.name as assign_name,
@@ -60,9 +55,9 @@ def assignment_grade(id, session_id, course_id):
 	           JOIN assignments on assignments.session_id=sessions.id
                JOIN grades on grades.assignment_id=assignments.assignment_id
                JOIN roster on roster.session_id=sessions.id
-               WHERE grades.assignment_id=% s
-               AND grades.student_id=% s""",
-                (id, user_id,))
+               WHERE grades.assignment_id= %s
+               AND grades.student_id= %s""",
+                (id, user_id))
 
     grade = cur.fetchone()
     cur.close()
@@ -75,25 +70,22 @@ def assignment_grade(id, session_id, course_id):
 @login_required
 @student_required
 def view_student_gradebook():
+    """returns a list of all courses student is in with links to see grades"""
 
     user_id = session.get('user_id')
     courses = []
     grades = []
-<<<<<<< HEAD
     con = db.get_db()
     cur = con.cursor()
-    cur.execute("""SELECT courses.course_id, courses.name AS class_name,
-== == == =
-    cur=db.get_db().cursor()
+
     cur.execute("""SELECT DISTINCT courses.course_id, courses.name AS class_name,
->>>>>>> 38fcd03cf03fb074bfe4016a0695b48dc5075de0
             users.name AS teacher, sessions.id AS session_id
             FROM roster JOIN sessions on roster.session_id = sessions.id
             JOIN courses on sessions.course_id = courses.course_id
             JOIN users on users.id = courses.teacherid
             WHERE roster.student_id =  %s """,
                 (user_id,))
-    courses=cur.fetchall()
+    courses = cur.fetchall()
     cur.close()
     con.close()
 
@@ -105,9 +97,15 @@ def view_student_gradebook():
 @login_required
 def view_grades_by_course(course_id, session_id):
 
-    user_id=session.get('user_id')
-
-    cur=db.get_db().cursor()
+    user_id = session.get('user_id')
+    # get the course info to display on page with grade information
+    cur = db.get_db().cursor()
+    cur.execute("""SELECT courses.name AS class_name,
+                users.name AS teacher
+                FROM courses JOIN users ON courses.teacherid = users.id
+                WHERE course_id=%s""",
+                (course_id,))
+    course_info = cur.fetchone()
 
     # Get all of the grades in the course for a student
     cur.execute("""
@@ -117,23 +115,24 @@ def view_grades_by_course(course_id, session_id):
     AND session_id = %s
     """, (user_id, session_id))
 
-    course_grade=cur.fetchall()
+    course_grade = cur.fetchall()
 
-    points_received=0
-    total_points=0
+    points_received = 0
+    total_points = 0
 
     # calculate the points for the grade
     for grade in course_grade:
 
-        total_points=total_points + grade['total_points']
-        points_received=points_received + grade['points_received']
+        total_points = total_points + grade['total_points']
+        points_received = points_received + grade['points_received']
 
     # if there's no assignments, students still have %100 grade
     if total_points == 0:
-        current_grade=100
+        current_grade = 100
     else:
-        current_grade=(points_received / total_points) * 100
+        current_grade = round((points_received / total_points), 2) * 100
 
+    # select all individual assignment grades in this course session
     cur.execute("""SELECT DISTINCT grades.grade_id, sessions.id,
             (ROUND(grades.points_received/grades.total_points, 2 )*100) as assignment_grade,
             grades.total_points as total, grades.points_received as earned,
@@ -143,13 +142,12 @@ def view_grades_by_course(course_id, session_id):
             JOIN grades on grades.assignment_id = assignments.assignment_id
             JOIN roster on roster.session_id = sessions.id
             WHERE grades.student_id = %s
-            AND courses.course_id = %s""",
-                (user_id, course_id))
-    assignment_grades=cur.fetchall()
+            AND sessions.id = %s""",
+                (user_id, session_id))
+    assignment_grades = cur.fetchall()
 
     cur.close()
-    con.close()
 
     return render_template("/layouts/gradebook/course_grades.html", course_id=course_id,
-    assignment_grades=assignment_grades, points_received=points_received, total_points=total_points,
-    current_grade=current_grade, course_grade=course_grade)
+                           assignment_grades=assignment_grades, points_received=points_received, total_points=total_points,
+                           current_grade=current_grade, course_grade=course_grade, session_id=session_id, course_info=course_info)
